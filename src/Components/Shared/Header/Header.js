9a9739b0b3c422e5c1b58e92414044c6eaea7e49
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-04-29 11:47:42 +0600

    nav responsive done

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomLink from '../CustomLink/CustomLink';
-import logo from '../../../logo.png'
+import logo from '../../../logo.png';
+import { MenuAlt2Icon, XCircleIcon } from '@heroicons/react/solid'
 import './Header.css'
 
 const Header = () => {
+    const [show, setShow] = useState(true);
+    const nav =
+        < nav >
+            <CustomLink to={'/bou'}>Home</CustomLink>
+            <CustomLink to={'/'}>Home</CustomLink>
+            <CustomLink to={'/'}>Home</CustomLink>
+            <CustomLink to={'/'}>Home</CustomLink>
+            <CustomLink to={'/'}>Home</CustomLink>
+        </nav >;
+
+
     return (
         <header>
             <div className="logo">
                 <img src={logo} alt="" />
             </div>
-            <nav>
-                <CustomLink to={'/bou'}>Home</CustomLink>
-                <CustomLink to={'/'}>Home</CustomLink>
-                <CustomLink to={'/'}>Home</CustomLink>
-                <CustomLink to={'/'}>Home</CustomLink>
-                <CustomLink to={'/'}>Home</CustomLink>
-            </nav>
+
+            <div className="menu">
+                {nav}
+            </div>
+
+            <div className="menu-icon">
+                {
+                    show ?
+                        <XCircleIcon style={{ width: '35px' }} onClick={() => setShow(!show)} />
+                        :
+                        <MenuAlt2Icon style={{ width: '30px' }} onClick={() => setShow(!show)} />
+                }
+            </div>
+
+            {show &&
+                <div className="sm-menu">
+                    {nav}
+                </div>
+            }
         </header>
     );
 };
