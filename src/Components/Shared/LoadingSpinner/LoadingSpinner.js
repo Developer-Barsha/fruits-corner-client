34222b6ac093004839a5d3e334059ea0d1c1ec82
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-05-07 00:09:44 +0600

    spinner problem solved

--- src/Components/Shared/LoadingSpinner/LoadingSpinner.js
+++ src/Components/Shared/LoadingSpinner/LoadingSpinner.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './LoadingSpinner.css'
 
 const LoadingSpinner = () => {
     return (
-        <div className='d-flex justify-content-center align-items-center mx-auto' >
-            <img className='d-flex justify-content-center align-items-center mx-auto' src="https://www.w3tweaks.com/images/essentials/css-loaders/window-icon-like-loader.gif" width={'300px'}alt="" />
-        </div>
+        <section className='w-75 position-relative' style={{minHeight:'300px'}}>
+            <div className='spinner d-flex justify-content-center align-items-center mx-auto' >
+                <img className='d-flex justify-content-center align-items-center mx-auto' src="https://www.w3tweaks.com/images/essentials/css-loaders/window-icon-like-loader.gif" width={'300px'} alt="" />
+            </div>
+        </section>
     );
 };
 
